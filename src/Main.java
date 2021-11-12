354adc58a4bba9c16ddeb0581804cf688266b70d
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2021-11-12 16:00:07 +0200

    Added sum in main

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        State initialstate = new State(100,1);
+        State initialstate = new State(10,3);
         Search search = new Search(100);
         long start = System.currentTimeMillis();
         State terminal = search.Astarsearch(initialstate);
@@ -21,12 +21,15 @@ public class Main {
             }
             //reverse the path
             Collections.reverse(path);
+            double sum = 0;
             for(State item: path){
+                sum = item.getCost_of_travel() + sum;
                 System.out.println(item);
             }
 
             System.out.println();
             System.out.println("Search time:" + (double) (end-start)/1000 + "sec");//total time in seconds
+            System.out.println("Total cost:" + sum);
         }
 
     }
